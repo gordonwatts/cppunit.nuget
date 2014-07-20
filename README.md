3c9354a4178b107f8831d69d77907e931e6e86f5
Gordon Watts <gordon@gordonandpaula.net>
2014-07-20 02:00:47 +0200

    Updates with a test driver soltuion (for now)

--- README.md
+++ README.md
@@ -9,9 +9,11 @@ Usage
 1. Create a VS2013 C++ console application. Make sure it is empty. No precompiled headers, etc.
 2. Add source files that contain test cases.
 3. Add this nuget package to the project.
-4. Build and run. You'll have a command line runner for this.
+4. Make sure a single source file includes the line:
+	#include <TestPolicy/CppUnit_testdriver.cxx>
+5. Build and run. You'll have a command line runner for this. It is no frills, but...
 
-Building
+Building a new nuget package
 ========
 
 These instructions assume you have installed locally:
@@ -21,6 +23,5 @@ These instructions assume you have installed locally:
 2. Download a source code snapshot of cppunit from source forge repo. Unpack it at the same level as this repo's directory (cd "ls ../cpunit" from this repo will give a listing.
 3. In the root directory there is a Build2010 solution file. Open it, and allow it to convert. It may not be able to convert the MSI file - that is ok.
 4. Build both the Release and the Debug configurations of the cppunit project (that is the only one required).
-
-XX. Update the description in the autopkg file to match the date of the last check in.
+5. Update the description in the autopkg file to match the date of the last check in.
 
---
+++ include/TestPolicy/CppUnit_testdriver.cxx
@@ -0,0 +1,48 @@
+#include <cppunit/extensions/TestFactoryRegistry.h> 
+#include <cppunit/TextTestRunner.h> 
+#include <cppunit/CompilerOutputter.h> 
+#include <cppunit/TextOutputter.h> 
+#include <cppunit/XmlOutputter.h> 
+#include <iostream>
+
+/**  Main class for all the CppUnit test classes
+*
+*  This will be the driver class of all your CppUnit test classes.
+*  - All registered CppUnit test classes will be run.
+*  - You can also modify the output (text, compiler, XML).
+*  - This class will also integrate CppUnit test with Oval
+*/
+
+
+int main(int /*argc*/, char **/* argv */)
+{
+	/// Get the top level suite from the registry
+	CppUnit::Test *suite = CppUnit::TestFactoryRegistry::getRegistry().makeTest();
+
+	/// Adds the test to the list of test to run
+	CppUnit::TextTestRunner runner;
+	runner.addTest(suite);
+
+	// Change the default outputter to a compiler error format outputter 
+	// uncomment the following line if you need a compiler outputter.
+	//runner.setOutputter(new CppUnit::CompilerOutputter( &runner.result(),
+	//                                                     std::cerr ) );
+
+	// Change the default outputter to a xml error format outputter 
+	// uncomment the following line if you need a xml outputter.
+	//runner.setOutputter( new CppUnit::XmlOutputter( &runner.result(),
+	//                                                    std::cerr ) );
+
+	/// Run the tests.
+	// bool wasSuccessful = runner.run_spi();
+	// If you want to avoid the CppUnit typical output change the line above 
+	// by the following one: 
+	bool wasSuccessful = runner.run("", false, true, false);
+
+	// Return error code 1 if the one of test failed.
+	if (!wasSuccessful) return 1;
+
+	// Uncomment the next line if you want to integrate CppUnit with Oval
+	//  std::cout <<"[OVAL] Cppunit-result ="<<!wasSuccessful<<"\n" ;
+	return 0;
+}
